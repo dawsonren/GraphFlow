7d9fd7f0b6be2db16cd2b1d7d1bea628176aa285
Dawson Ren <dawsonren@gmail.com>
2021-11-10 12:32:31 -0600

    Fix routing

---
+++ controllers/graphController.js
@@ -0,0 +1,46 @@
+var Graph = require('../models/graph');
+
+// Display the list of all Graphs
+exports.graph_list = function(req, res) {
+  res.render('graph', { title: 'GraphFlow' });
+}
+
+// Display the list of all Nodes.
+exports.node_list = function(req, res) {
+  res.send('NOT IMPLEMENTED: Node list')
+}
+
+// Display the list of all Edges.
+exports.edge_list = function(req, res) {
+  res.send('NOT IMPLEMENTED: Edge list')
+}
+
+// Display Graph create form on GET
+exports.graph_create_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph create GET')
+}
+
+// Handle Graph create on POST
+exports.graph_create_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph create POST')
+}
+
+// Display Graph delete form on GET
+exports.graph_delete_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph delete GET')
+}
+
+// Handle Graph delete on POST
+exports.graph_delete_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph delete POST')
+}
+
+// Display Graph update form on GET
+exports.graph_update_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph update GET')
+}
+
+// Handle Graph delete on POST
+exports.graph_update_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: Graph update POST')
+}
