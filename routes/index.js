8f73c65d417690841600701ef0559b711cecd2ae
Dawson Ren <dawsonren@gmail.com>
2021-11-10 12:04:19 -0600

    HI NADHARM

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/graphs')
 });
+
+router.get('/graphs', function(req, res, next) {
+  res.render('graph', { title: 'GraphFlow' });
+})
+
 
 module.exports = router;
