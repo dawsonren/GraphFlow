e9c0cb249e969215ed6597159f011169c37d7fd6
Dawson Ren <dawsonren@gmail.com>
2021-11-30 09:53:33 -0600

    Revert "Broken, API calls somehow don't go thorugh"
    This reverts commit 32eb755a2788884a693ec6ced2d4d73d637ef5a0.

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -12,7 +12,6 @@ export const LandingScreen = () => {
 
   async function goToCreate() {
     if (user.uuid) {
-      console.log(user.uuid)
       const graph = await createGraph('Untitled Graph', user.uuid)
       await navigate(`/graph/${graph._id}`)
     } else {
