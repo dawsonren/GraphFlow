9443c6dd38d7cd72bfb9b389b986a0b6c255b0a2
Dawson Ren <dawsonren@gmail.com>
2021-12-02 16:26:57 -0600

    help

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join('frontend/public')));
 
 // Passport middleware
 app.use(passport.initialize());
